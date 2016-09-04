862fdc6c5ec22a7a586be4f1d4d7c50098a54f7d
Jeff Hammond <jeff.science@gmail.com>
2016-09-04 15:30:45 -0700

    fix test

--- test/test_new_gcc_intrinsics.c
+++ test/test_new_gcc_intrinsics.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include <assert.h>
 
 int main(int argc, char **argv)
 {
-    __atomic_int a, b;
+    int a, b;
     int c;
 
     //OPA_store_int(&a, 0);
@@ -15,16 +16,17 @@ int main(int argc, char **argv)
     __atomic_store_n(&b, 1, __ATOMIC_RELAXED);
 
     //OPA_add_int(&a, 10);
-    __atomic_fetch_add_n(&a, 10, __ATOMIC_RELAXED);
+    __atomic_fetch_add(&a, 10, __ATOMIC_RELAXED);
 
     //assert(10 == OPA_load_int(&a));
     assert(10 == __atomic_load_n(&a, __ATOMIC_RELAXED));
 
     //c = OPA_cas_int(&a, 10, 11);
     {
+      bool weak = false;
       int old = 10;
       int new = 11;
-      __atomic_compare_exchange_n(&a, &old, new,
+      __atomic_compare_exchange_n(&a, &old, new, weak,
                                   __ATOMIC_RELAXED,
                                   __ATOMIC_RELAXED);
       c = old;
